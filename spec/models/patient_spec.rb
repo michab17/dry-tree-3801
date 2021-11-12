c861a41a2278f9a03df53b72c0bf0ef3a9d10b18
Micha Bernhard <mib70@pitt.edu>
2021-11-12 14:48:36 -0700

    Test: add model test for by_age method

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Patient do
     it { should have_many(:doctor_patients) }
     it { should have_many(:doctors).through(:doctor_patients) }
   end
+  
+  describe '::by_age' do
+    it 'orders all patients by age' do
+      hospital = Hospital.create!(name: "Good Hospital")
+      doctor = hospital.doctors.create!(name: "Dr", specialty: "General Surgery", university: "Stanford University")
+      patient1 = Patient.create!(name: 'Bob', age: 26)
+      patient2 = Patient.create!(name: 'John', age: 27)
+      DoctorPatient.create!(doctor_id: doctor.id, patient_id: patient1.id)
+      DoctorPatient.create!(doctor_id: doctor.id, patient_id: patient2.id)
+
+      expect(Patient.by_age).to eq([patient2, patient1])
+    end
+  end
 end
