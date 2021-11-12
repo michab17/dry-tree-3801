d5862d16bbce4ba14325ceec19eb88248400500b
Micha Bernhard <mib70@pitt.edu>
2021-11-12 13:10:40 -0700

    update tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_12_200308) do
+ActiveRecord::Schema.define(version: 2021_11_12_200936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2021_11_12_200308) do
     t.index ["hospital_id"], name: "index_doctors_on_hospital_id"
   end
 
+  create_table "doctors_patients", force: :cascade do |t|
+    t.bigint "doctor_id"
+    t.bigint "patient_id"
+    t.index ["doctor_id"], name: "index_doctors_patients_on_doctor_id"
+    t.index ["patient_id"], name: "index_doctors_patients_on_patient_id"
+  end
+
   create_table "hospitals", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -34,12 +41,9 @@ ActiveRecord::Schema.define(version: 2021_11_12_200308) do
   create_table "patients", force: :cascade do |t|
     t.string "name"
     t.integer "age"
-    t.bigint "doctor_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["doctor_id"], name: "index_patients_on_doctor_id"
   end
 
   add_foreign_key "doctors", "hospitals"
-  add_foreign_key "patients", "doctors"
+  add_foreign_key "doctors_patients", "doctors"
+  add_foreign_key "doctors_patients", "patients"
 end
