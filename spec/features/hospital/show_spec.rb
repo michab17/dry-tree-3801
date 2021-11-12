dbf6dcc9e6ed464ebeb574c0e52220f0191f65ae
Micha Bernhard <mib70@pitt.edu>
2021-11-12 14:11:00 -0700

    Test: Add .once to test uniqueness

--- spec/features/hospital/show_spec.rb
+++ spec/features/hospital/show_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'Hospital Show Page' do
 
     visit hospital_path(hospital.id)
 
+    expect(page).to have_content('Name:')
     expect(page).to have_content('Good Hospital')
   end
 
@@ -23,11 +24,12 @@ RSpec.describe 'Hospital Show Page' do
     hospital = Hospital.create!(name: "Good Hospital")
     doctor1 = hospital.doctors.create!(name: "Dr", specialty: "General Surgery", university: "Stanford University")
     doctor2 = hospital.doctors.create!(name: "Doc", specialty: "General Surgery", university: "University of Pittsburgh")
+    doctor2 = hospital.doctors.create!(name: "Doctor", specialty: "General Surgery", university: "University of Pittsburgh")
 
     visit hospital_path(hospital.id)
-
+    save_and_open_page
     expect(page).to have_content('Universities:')
     expect(page).to have_content('Stanford University')
-    expect(page).to have_content('University of Pittsburgh')
+    expect(page).to have_content('University of Pittsburgh').once
   end
 end
