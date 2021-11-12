e773085ac85ba5e04b282ab0af23afa67ba30a53
Micha Bernhard <mib70@pitt.edu>
2021-11-12 14:16:43 -0700

    Add model test for universities method

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe Hospital do
   describe 'relationships' do
     it { should have_many(:doctors) }
   end
+
+  describe '#universities' do
+    it 'returns a unique list of universitys at a hospital' do
+      hospital = Hospital.create!(name: "Good Hospital")
+      doctor1 = hospital.doctors.create!(name: "Dr", specialty: "General Surgery", university: "Stanford University")
+      doctor2 = hospital.doctors.create!(name: "Doc", specialty: "General Surgery", university: "University of Pittsburgh")
+      doctor2 = hospital.doctors.create!(name: "Doctor", specialty: "General Surgery", university: "University of Pittsburgh")
+      
+      expect(hospital.universities.length).to eq(2)
+      expect(hospital.universities[0].university).to eq('Stanford University')
+      expect(hospital.universities[1].university).to eq('University of Pittsburgh')
+    end
+  end
 end
